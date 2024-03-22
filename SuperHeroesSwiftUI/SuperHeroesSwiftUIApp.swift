e3351d729f2adcab98276e1c73f68183be12b01d
David Rubio <davidrubioc@gmail.com>
2024-03-22 23:16:06 +0100

    Initial Commit

---
+++ SuperHeroesSwiftUI/SuperHeroesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroesSwiftUIApp.swift
+//  SuperHeroesSwiftUI
+//
+//  Created by David Rubio on 22/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
